154afae3f970b1abeea95211a065834fe8c35d80
Nathan <me@nwilce.co.uk>
2023-08-31 17:00:20 +0000

    copy trainer.optimizer to model.optimizer

--- RWKV-v5/src/rwkv_cli.py
+++ RWKV-v5/src/rwkv_cli.py
@@ -4,3 +4,4 @@ from lightning.pytorch.cli import LightningCLI
 class RWKVLightningCLI(LightningCLI):
     def add_arguments_to_parser(self, parser):
         parser.add_argument("--trainer.optimizer", default="adam")
+        parser.link_arguments("trainer.optimizer", "model.optimizer")
